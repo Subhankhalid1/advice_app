b2649b6955341dc0646500cd245016c003b80356
Subhankhalid1 <rsubhakhalid@gmail.com>
2020-06-24 05:16:34 +0500

    final advice app
    Advice app using fetch API in a very simple way

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+class App extends React.Component {
+  state = {
+    advice: ""
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  componentDidMount() {
+    this.fetchAdvice();
+    console.log("I'm Mounted")
+  }
+
+
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+
+      .then((resolve) => {
+        const { advice } = resolve.data.slip;
+        console.log(advice);
+        this.setState({ advice })
+      })
+
+      .catch((error) => {
+        console.log(error);
+      })
+
+  }
+
+
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button onClick={this.fetchAdvice} className="button"><span>Give me advice</span></button>
+        </div>
+        <div className="copyright"> Copyright © 2020 Rana Subhan, LLC dba AutoBots </div> 
+      </div>
+    );
+
+
+  }
+
 }
+
+
 
 export default App;
